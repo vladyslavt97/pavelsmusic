7f2498aa9eb476c782029614727a9a8fc8ce289d
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-06-03 12:58:57 +0200

    excalamation mark

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='text-2xl flex flex-col justify-center items-center h-screen'>
-      <p>Pavel Music Website</p>
+      <p>Pavel Music Website!</p>
       <p className='text-sm'>under constraciton...</p>
     </main>
   )
