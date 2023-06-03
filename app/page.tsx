9f8f9f8db221365d7f43eea19f416b8a6f34d2ac
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-06-03 14:09:51 +0200

    md: and gradient

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className='text-2xl flex flex-col justify-center items-center h-screen'>
-      <p>Pavel Music Website!</p>
-      <p className='text-sm'>under constraciton...</p>
+      <p className='bg-clip-text text-transparent bg-gradient-to-r from-red-500 via-yellow-500 to-green-500 text-bold md:text-4xl'>Pavel Music Website!</p>
+      <p className='text-sm animate-pulse md:text-lg'>under construction...</p>
     </main>
   )
 }
