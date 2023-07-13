1069929b63b1c3273febec054e6704068f6e16be
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-07-13 20:00:49 +0200

    button updated and created bio component

---
+++ app/components/Bio.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Bio({}: Props) {
+  return (
+    <p className="text-white w-4/5 md:w-2/3 mt-52 font-sans mb-32">
+        Pavel Kuznetsov studied piano at Universität der Künste Berlin with Professor Elena Lapitskaja and Mrs. Mi-Joo Lee. (since 2012).
+        Since 2006, he has regularly participated in various international competitions.
+    </p>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className='text-2xl flex flex-col justify-center items-center h-screen bg-black'>
+    <main className='text-2xl flex flex-col justify-center items-center bg-black h-screen'>
       <Header/>
-      <p className='text-white text-bold text-[50px] uppercase'>Pavels/Music</p>
-      <h5 className="text-white font-serif text-[20px] mt-10">Listen on </h5>
 
-      <div>
-        <Link href="https://music.apple.com/us/artist/pavels-music/1608194615"><Image src="/apple1.png" width={100} height={100} alt="apple" className="py-1"/></Link>
-        <Image src="/spotifyy.png" width={100} height={100} alt="apple" className="py-1"/>
-        <Image src="/amazon1.png" width={100} height={100} alt="apple" className="py-1"/>
-        <Image src="/deezer.png" width={100} height={100} alt="apple" className="py-1"/>
+      <div className="relative top-28 flex justify-center flex-col items-center">
+        <p className='text-white text-bold text-[50px] uppercase'>Pavels/Music</p>
+      
+        <h5 className="text-white font-serif text-[20px] mt-10">Listen on </h5>
+
+        <div className="">
+          <Link href="https://music.apple.com/us/artist/pavels-music/1608194615"><Image src="/apple1.png" width={100} height={100} alt="apple" className="py-1"/></Link>
+          <Image src="/spotifyy.png" width={100} height={100} alt="apple" className="py-1"/>
+          <Image src="/amazon1.png" width={100} height={100} alt="apple" className="py-1"/>
+          <Image src="/deezer.png" width={100} height={100} alt="apple" className="py-1"/>
+        </div>
       </div>
       
     </main>
