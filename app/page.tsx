215ea7786414de67096ee380598d97a4306200a8
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-07-24 18:50:22 +0200

    motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+"use client"
 import Link from "next/link";
 import Header from "./components/Header";
 import Image from "next/image";
+import { motion } from "framer-motion"
+import links from "../links.json"
+
 
 export default function Home() {
   return (
@@ -8,16 +12,38 @@ export default function Home() {
       <Header/>
 
       <div className="flex justify-center flex-col items-center">
-        <p className='text-white text-bold text-[40px] sm:text-[50px] uppercase'>Pavels/Music</p>
-      
-        <h5 className="text-white font-serif text-[20px] mt-10">Listen on </h5>
+        <motion.p 
+        initial={{opacity:0}}
+        animate={{opacity:1}}
+        transition={{duration:2}}
+        className='text-white text-bold text-[40px] sm:text-[50px] uppercase'>Pavels/Music</motion.p>
+
+        <motion.div
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{duration:3}}>
+          <h5 className="text-white font-serif text-[20px] mt-10">Listen on </h5>
+        </motion.div>
+
+
 
         <div className="flex flex-col items-center">
-          <Link href="https://music.apple.com/us/artist/pavels-music/1608194615"><Image src="/apple1.png" width={100} height={100} alt="apple" className="py-1"/></Link>
-          <Link href="https://open.spotify.com/artist/10yXiUlSrROMsEUN2tn4vo?si=sRymkUglTWK_9CEi2XVL1A"><Image src="/spotifyy.png" width={100} height={100} alt="spotify" className="py-1"/></Link>
-          <Link href="https://music.amazon.de/artists/B09RQ38ZBN?ref=dm_sh_JlWevsn20wYn3FnxvgSkTna2g"><Image src="/amazon1.png" width={100} height={100} alt="anazon" className="py-1"/></Link>
-          <Link href="https://deezer.page.link/WGMjU9M1xkFXY36K7"><Image src="/deezer.png" width={100} height={100} alt="deezer" className="py-1"/></Link>
-          <Link href="https://music.youtube.com/channel/UCrVMHk4yP7q_hcRgwGJyVtA?feature=share"><Image src="/youtube-music1.png" width={150} height={100} alt="youtube" className="py-1"/></Link>
+          {links.map((el, index) => (
+            <motion.div
+            key={index}
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{duration:2, delay: el.delay}}>
+            <Link href={el.linik}>
+              <Image src={el.image} width={el.size} height={100} alt="image" className="py-1"/>
+            </Link>
+          </motion.div>
+          ))}
+          
+
+          {/* 
+
+          <Link href=""><Image src="" width={150} height={100} alt="youtube" className="py-1" loading="eager"/></Link> */}
         </div>
       </div>
       
