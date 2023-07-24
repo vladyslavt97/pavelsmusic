b9c2b076b453098fb0d663dfb9b73244b225baa7
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-07-24 18:34:11 +0200

    font and links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import Header from './components/Header'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Rubik } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Rubik({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Pavel Music',
--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className='text-2xl flex flex-col justify-center items-center bg-black h-screen'>
+    <main className='text-2xl flex flex-col justify-center items-center bg-black min-h-screen'>
       <Header/>
 
-      <div className="relative top-28 flex justify-center flex-col items-center">
-        <p className='text-white text-bold text-[50px] uppercase'>Pavels/Music</p>
+      <div className="flex justify-center flex-col items-center">
+        <p className='text-white text-bold text-[40px] sm:text-[50px] uppercase'>Pavels/Music</p>
       
         <h5 className="text-white font-serif text-[20px] mt-10">Listen on </h5>
 
-        <div className="">
+        <div className="flex flex-col items-center">
           <Link href="https://music.apple.com/us/artist/pavels-music/1608194615"><Image src="/apple1.png" width={100} height={100} alt="apple" className="py-1"/></Link>
-          <Link href=""><Image src="/spotifyy.png" width={100} height={100} alt="apple" className="py-1"/></Link>
-          <Link href=""><Image src="/amazon1.png" width={100} height={100} alt="apple" className="py-1"/></Link>
-          <Link href=""><Image src="/deezer.png" width={100} height={100} alt="apple" className="py-1"/></Link>
+          <Link href="https://open.spotify.com/artist/10yXiUlSrROMsEUN2tn4vo?si=sRymkUglTWK_9CEi2XVL1A"><Image src="/spotifyy.png" width={100} height={100} alt="spotify" className="py-1"/></Link>
+          <Link href="https://music.amazon.de/artists/B09RQ38ZBN?ref=dm_sh_JlWevsn20wYn3FnxvgSkTna2g"><Image src="/amazon1.png" width={100} height={100} alt="anazon" className="py-1"/></Link>
+          <Link href="https://deezer.page.link/WGMjU9M1xkFXY36K7"><Image src="/deezer.png" width={100} height={100} alt="deezer" className="py-1"/></Link>
+          <Link href="https://music.youtube.com/channel/UCrVMHk4yP7q_hcRgwGJyVtA?feature=share"><Image src="/youtube-music1.png" width={150} height={100} alt="youtube" className="py-1"/></Link>
         </div>
       </div>
       
