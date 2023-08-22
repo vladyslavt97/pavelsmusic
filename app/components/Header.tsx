b775994b11484e940346b18c8155b15ef0b6cff6
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-08-22 11:38:54 +0200

    shadow on the menu button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,6 +3,6 @@ type Props = {}
 
 export default function Header({}: Props) {
   return (
-    <div className="text-white bg-gradient-to-tr from-blue-200 to-blue-500 px-2 rounded-full absolute top-3 md:top-10 left-3 md:right-10 text-2xl w-20">Menu</div>
+    <div className="text-white bg-gradient-to-tr from-blue-200 to-blue-500 px-2 rounded-full absolute top-3 md:top-10 left-3 md:right-10 text-2xl w-20 shadow-md  shadow-white">Menu</div>
   )
 }
